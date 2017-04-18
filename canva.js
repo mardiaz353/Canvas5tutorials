8e5bd3f1e568a9f8f472588a7c5b483c2c506f05
mardiaz353 <adiaz064@gmail.com>
2017-04-17 23:28:30 -0700

    canva5tutorials
    I watched 3 bucky videos about canva 5. Tomorrow i can continue

---
+++ canva.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+context.font = '32pt Arial';
+context.fillStyle = 'DeepSkyBlue';
+context.strokeStyle = 'DarkSlateGray';
+
+context.fillText('Cant wait to use canva?', 30, 200);
+context.strokeText('Cant wait to use canva?', 30, 200);
